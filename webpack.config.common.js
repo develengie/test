3c4be3407014082ac321f79714cbc925e90c7af5
Alexey <alexeyushakov1997@yandex.ru>
2025-04-10 18:34:22 +0300

    build(config): подключил аудио

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -46,6 +46,10 @@ module.exports = {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
                 type: 'asset/resource',
             },
+            {
+                test: /\.(mp3)$/i,
+                type: 'asset/resource',
+            },
         ],
     },
 };
