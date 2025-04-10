69e1ae28462d8e7aff757ba38a0a53b985e42b9f
Alexey <alexeyushakov1997@yandex.ru>
2025-04-10 19:22:44 +0300

    build(config): подключил CssMinimizerWebpackPlugin

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,10 @@
 const { merge } = require('webpack-merge');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const commonConfig = require('./webpack.config.common');
 
 module.exports = merge(commonConfig, {
     mode: 'production',
+    optimization: {
+        minimizer: [new CssMinimizerPlugin()],
+    },
 });
