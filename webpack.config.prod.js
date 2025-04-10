54c8ddd8e795d0cd8861c72745e7a75f573c5e0e
Alexey <alexeyushakov1997@yandex.ru>
2025-04-10 19:28:17 +0300

    build(config): подключил TerserWebpackPlugin

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,10 +1,12 @@
 const { merge } = require('webpack-merge');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const commonConfig = require('./webpack.config.common');
 
 module.exports = merge(commonConfig, {
     mode: 'production',
     optimization: {
-        minimizer: [new CssMinimizerPlugin()],
+        minimize: true,
+        minimizer: [new CssMinimizerPlugin(), new TerserPlugin()],
     },
 });
