732a7fc0338e71dbb6370f57ab48b414d0fdb00b
Alexey <alexeyushakov1997@yandex.ru>
2025-04-10 20:16:51 +0300

    build(config): подключил EslintWebpackPlugin

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,4 +1,5 @@
 const { merge } = require('webpack-merge');
+const ESLintPlugin = require('eslint-webpack-plugin');
 const commonConfig = require('./webpack.config.common');
 
 module.exports = merge(commonConfig, {
@@ -8,4 +9,5 @@ module.exports = merge(commonConfig, {
         hot: true,
         open: true,
     },
+    plugins: [new ESLintPlugin()],
 });
